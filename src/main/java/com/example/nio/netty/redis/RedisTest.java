69cab58f3a3f952a0a372c80e73b5483fc52848f
Chihyao Huang <chihyao.huang@kangdainfo.com>
2022-02-13 15:38:44 +0800

    add netty protobuf with redis

---
+++ src/main/java/com/example/nio/netty/redis/RedisTest.java
@@ -0,0 +1,73 @@
+package com.example.nio.netty.redis;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.nio.charset.Charset;
+
+public class RedisTest {
+
+    public static void main(String[] args) {
+        final byte[] LINE = {13, 10};
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.group(worker);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel socketChannel) throws Exception {
+                    socketChannel.pipeline().addLast(new LoggingHandler());
+                    socketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            ByteBuf buf = ctx.alloc().buffer();
+                            /**
+                             * 與 redis 協議規則
+                             * set key value
+                             * 1.告訴 redis 有幾個字節 (set key value共3個, 輸入*3)
+                             * 2.第一個字節(set)有幾個字元 (set 有3個, 輸入$3)
+                             * 3.第二個字節(key)有幾個字元 (name 有4個, 輸入$4)
+                             * 4.第三個字節(value)有幾個字元 (Frank 有5個, 輸入$5)
+                             **/
+                            buf.writeBytes("*3".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("$3".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("set".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("$4".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("name".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("$5".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("Frank".getBytes());
+                            buf.writeBytes(LINE);
+                            ctx.writeAndFlush(buf);
+                        }
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            ByteBuf buf = (ByteBuf) msg;
+                            System.out.println(buf.toString(Charset.defaultCharset()));;
+                        }
+                    });
+                }
+            });
+            ChannelFuture future = bootstrap.connect("localhost", 6379).sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            worker.shutdownGracefully();
+        }
+    }
+}
